72da1d07ba3e75a1a81dfc7df7671b3b1527e481
Eric Kobrin <erluko@gmail.com>
2015-01-31 14:30:48 -0500

    seems to work

--- abs_path.c
+++ abs_path.c
@@ -16,6 +16,19 @@ abs_path (const char *base, const char *rel)
   int eat;
   char c;
 
+  if(rel == NULL && base == NULL) {
+    errno = EINVAL;
+    return NULL;
+  }
+
+  if(rel == NULL){
+    rel = "";
+  }
+
+  if(rel[0] == '/' || base == NULL) {
+    base = "";
+  }
+
   ebase = base + strnlen(base, PATH_MAX);
   if(*ebase != 0) {
     errno = ENAMETOOLONG;
@@ -34,12 +47,52 @@ abs_path (const char *base, const char *rel)
   *op = 0;
 
   /* -------------------------------*/
+  eat = 0;
+  ahead = 1;
+
   targ = rel;
   for(ip=erel-1; ip >= targ;
       ip = targ == rel && ip == targ? ( targ=base, ebase ) :  (ip-1)){
     c = ip==ebase?'/':*ip;
     printf("%c",c);
+    if(ahead > 0){
+      if(ahead <= 3){
+        if(c == '/'){
+          if(ahead == 1) {
+            continue; /* consume runs of slashes */
+          }else if(ahead == 2) {
+            ahead = 1; /* ./ */
+            continue;
+          } else { /* ../ */
+            eat++;
+            ahead = 1;
+            continue;
+          }
+        } else if (c == '.') {
+          ahead++;
+          continue;
+        }
+      }
+      while(--ahead > 0){
+        if(eat == 0) *(--op)='.';
+      }
+      if(eat == 0) *(--op) = c;
+    } else {
+      if(eat == 0) *(--op) = c;
+      if(c == '/'){
+        if(eat>0) eat--;
+        ahead = 1;
+      }
+    }
   }
+  if(op>out){
+    printf("\nX:%p\nY:%p",(void*)op, (void*)out);
+    osize = out+osize-op;
+    memmove(out, op, osize);
+    out[osize]=0;
+    realloc(out, osize);
+  }
+  printf("\n>>%s<<\n",out);
   return NULL;
 }
 
