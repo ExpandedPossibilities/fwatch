6f06f2cee66bc4265c8073fb63da0d5e8805bc26
Eric Kobrin <erluko@gmail.com>
2015-02-22 00:12:03 -0500

    test generator cleanup

--- tests/genpaths.pl
+++ tests/genpaths.pl
@@ -1,32 +1,31 @@
 #!/usr/bin/perl
 use strict;
 
-sub print_test{
+sub print_test {
   my ($s, $p) = (@_);
-  my $sa=$s?$s:'NONE';
-  my $pa=$p?$p:'NONE';
+  my $sa = $s? $s: 'NONE';
+  my $pa = $p? $p: 'NONE';
   $p="$s/$p" unless $p=~m:^/:;
   $p=~s:/+:/:g;
   my $ap = $p;
   while(
-    $ap=~s://+:/:g ||
-    $ap=~s:(^|/)\.(?=/|$):\1: ||
-    $ap=~s:(^|/)[^/]*/\.\.(?=/|$):\1:){}
-  $ap='/'.$ap;
-  while(
-    $ap=~s:^(/+\.\./)+:/:){};
-  $ap=~s:/+:/:g;
-  $ap=~s:(.)/$:\1:;
+    $ap =~ s://+:/:g ||
+    $ap =~ s:(^|/)\.(?=/|$):\1: ||
+    $ap =~ s:(^|/)[^/]*/\.\.(?=/|$):\1:){}
+  $ap = '/' . $ap;
+  while($ap =~ s:^(/+\.\./)+:/:){};
+  $ap =~ s:/+:/:g;
+  $ap =~ s:(.)/$:\1:;
   print "$sa\t$pa\t$ap\n";
 }
 
 sub encode {
-  my ($n,@a) = @_;
+  my ($n, @a) = @_;
   my $max = @a;
   my @o;
-  while($n > 0) {
+  while($n > 0){
     my $r = $n % $max;
-    push @o,$a[$r];
+    push @o, $a[$r];
     $n -= $r;
     $n /= $max;
   }
@@ -34,48 +33,48 @@ sub encode {
 }
 my $abusefirst = 0;
 
-sub gentemplates{
+sub gentemplates {
   my ($seed, $tries, $mod) = @_;
   my %all;
   my @l;
-  foreach my $i (0..$tries) {
-    my @comps = encode(($seed*$i) % $mod,'','d','.','d','..','d');
-    my $p = (join '/',  @comps);
+  foreach my $i (0..$tries){
+    my @comps = encode(($seed * $i) % $mod, '', 'd', '.', 'd', '..', 'd');
+    my $p = (join '/', @comps);
     my @versions;
     push @versions, $p;
-    my $num = $p=~/d/;
-    if($abusefirst) {
+    my $num = $p =~ /d/;
+    if($abusefirst){
       for(0..$num){
-        $p=~s:/d:/..d:;
+        $p =~ s:/d:/..d:;
         push @versions, $p;
       }
       $p = $versions[0];
       for(0..$num){
-        $p=~s:d/:d../:;
+        $p =~ s:d/:d../:;
         push @versions, $p;
       }
       $p = $versions[0];
       for(0..$num){
-        $p=~s:d/:d./:;
+        $p =~ s:d/:d./:;
         push @versions, $p;
       }
       $p = $versions[0];
       for(0..$num){
-        $p=~s:/d:.d/:;
+        $p =~s :/d:.d/:;
         push @versions, $p;
       }
       $p = $versions[0];
       for(0..$num){
-        $p=~s:/d/:/.d./:;
+        $p =~ s:/d/:/.d./:;
         push @versions, $p;
       }
       $p = $versions[0];
       for(0..$num){
-        $p=~s:/d/:/..d../:;
+        $p =~ s:/d/:/..d../:;
         push @versions, $p;
       }
     }
-    foreach my $s (@versions) {
+    foreach my $s (@versions){
       unless($all{$s}){
         push @l, $s;
         $all{$s} = 1;
@@ -85,31 +84,23 @@ sub gentemplates{
   return @l;
 }
 
-#print join "\n", gentemplate(1234,3000,5000);
-#print "\n";
-#exit 1;
-
 sub genpaths {
-  my ($seed,$tries,$mod,@parts) = @_;
-  my @templates = gentemplates($seed,$tries,$mod);
-  for(@templates) {
+  my ($seed, $tries, $mod, @parts) = @_;
+  my @templates = gentemplates($seed, $tries, $mod);
+  for(@templates){
     my $c = 0;
     s/d/$parts[$c++ % @parts]/ge;
   }
   return @templates;
 }
 
-#print join "\n", genpaths(123,3000,5000,qw/a b c e/);
-#print "\n";
-#exit 1;
+print_test('/', '');
+print_test('', '/');
 
-print_test('/','');
-print_test('','/');
-
-foreach my $base (genpaths(123,31,703,qw(foxtrot golf hotel)),
-                  genpaths(123,17,703,qw(foxtrot.. ..golf .hotel))) {
-  foreach my $path (genpaths(423,73,947,qw(alpha beta gamma ...)),
-                    genpaths(423,23,947,qw(alpha.. ..beta.. .gamma))) {
+foreach my $base (genpaths(123, 31, 703, qw(foxtrot golf hotel)),
+                  genpaths(123, 17, 703, qw(foxtrot.. ..golf .hotel))){
+  foreach my $path (genpaths(423, 73, 947, qw(alpha beta gamma ...)),
+                    genpaths(423, 23, 947, qw(alpha.. ..beta.. .gamma))){
     print_test($base, $path);
   }
 }
